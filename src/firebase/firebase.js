31da799dbb21a163f35f782181cad35c75510327
Alex Yakubin <a.yakubin@hymnai.com>
2020-12-16 19:12:39 +0200

    firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 export const app = firebase.app.length
 	? firebase.initializeApp({
-			apiKey: 'AIzaSyCxnZfNasYVfUnCZT8DIVg-9zXQfbqhFLE',
-			authDomain: 'dominvest-73638.firebaseapp.com',
-			databaseURL: 'https://dominvest-73638.firebaseio.com',
-			projectId: 'dominvest-73638',
-			storageBucket: 'dominvest-73638.appspot.com',
-			messagingSenderId: '887011249427',
-			appId: '1:887011249427:web:1b89bd5dcf335011bc9ee6',
-			measurementId: 'G-GP9MP3KPLV'
+		apiKey: 'AIzaSyCxnZfNasYVfUnCZT8DIVg-9zXQfbqhFLE',
+		authDomain: 'dominvest-73638.firebaseapp.com',
+		databaseURL: 'https://dominvest-73638.firebaseio.com',
+		projectId: 'dominvest-73638',
+		storageBucket: 'dominvest-73638.appspot.com',
+		messagingSenderId: '887011249427',
+		appId: '1:887011249427:web:1b89bd5dcf335011bc9ee6',
+		measurementId: 'G-GP9MP3KPLV'
 	  })
 	: null;
